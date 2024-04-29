4bd766678e293411a01cf565a30e74ac2f9733b5
stonefeld <theo.stanfield@gmail.com>
2024-04-29 19:40:49 -0300

    Faltaba el material unlit

--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -8,7 +8,7 @@ public static class TextureGenerator
     public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height)
     {
         Texture2D texture = new Texture2D(width, height);
-        texture.filterMode = FilterMode.Point;
+        texture.filerMode = FilterMode.Point;
         texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(colourMap);
         texture.Apply();
