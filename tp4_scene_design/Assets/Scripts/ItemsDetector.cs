c0983763a9f2df18fb2006804b259bf9a3a7612f
stonefeld <theo.stanfield@gmail.com>
2024-07-08 18:48:29 -0300

    Audios extras para 5 y 9 cristales

--- tp4_scene_design/Assets/Scripts/ItemsDetector.cs
+++ tp4_scene_design/Assets/Scripts/ItemsDetector.cs
@@ -7,6 +7,10 @@ public class ItemsDetector : MonoBehaviour
 {
     public GameObject crate;
     public TextMeshPro pointsText;
+    public AudioSource introAudioSource;
+    public AudioSource primerCristalAudioSource;
+    public AudioSource quintoCristalAudioSource;
+    public AudioSource novenoCristalAudioSource;
 
     private Dictionary<string, GameObject> foundCrystals = new Dictionary<string, GameObject>();
 
@@ -15,6 +19,27 @@ public class ItemsDetector : MonoBehaviour
         GameObject o = collider.gameObject;
         if (!foundCrystals.ContainsKey(o.name)) {
             foundCrystals.Add(o.name, o);
+
+            switch (foundCrystals.Count) {
+                case 1:
+                    introAudioSource.Stop();
+                    primerCristalAudioSource.Play();
+                    break;
+
+                case 5:
+                    introAudioSource.Stop();
+                    primerCristalAudioSource.Stop();
+                    quintoCristalAudioSource.Play();
+                    break;
+
+                case 9:
+                    introAudioSource.Stop();
+                    primerCristalAudioSource.Stop();
+                    quintoCristalAudioSource.Stop();
+                    novenoCristalAudioSource.Play();
+                    break;
+            }
+
 
             pointsText.text = "";
             foreach (string crystal in foundCrystals.Keys) {
